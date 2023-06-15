de2ad6589e838921d5d1eaf834e17ace8f81339d
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2023-06-15 11:49:26 -0400

    Added missing functions to header

--- include/joemat.h
+++ include/joemat.h
@@ -5,11 +5,15 @@ void setLieAlgebra(const char* s);
 
 int getLieAlgebraDim();
 const char* getLieAlgebraBasis();
+const char* getLieAlgebraCentralizer();
+const char* getLieAlgebraNormalizer();
 
 // Two options: Either pass in the Lie algebra for each computation
 // or set the Lie algebra before the computation
 int getLieAlgebraDim(const char* s);
 const char* getLieAlgebraBasis(const char* s);
+const char* getLieAlgebraCentralizer(const char* s);
+const char* getLieAlgebraNormalizer(const char* s);
 
 // int getMatrixRank(const char* s);
 // int getZero();
