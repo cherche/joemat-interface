ee3065cffa8f298963eedbad86d9a478096b77f2
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2023-06-27 16:34:12 -0400

    Fixed parsing bug in matrixToString
    Previously, symbolic entries such as `a + 1`
    would keep their spaces when converted into strings.
    This would mean the final string representation of
    our matrix would not be correctly formatting.
    (Here, correct formatting means single spaces between entries;
    semicolons between rows; square brackets "wrapping" everything together;
    and no extra characters.)

--- mlfunctions/matrixToString.m
+++ mlfunctions/matrixToString.m
@@ -1,2 +1,9 @@
 function stringRep = toString(mat)
-stringRep = replace(mat2str(string(mat)), '"', '')
+% We need to manually remove the spaces from each entry,
+% particularly when it comes to symbolic entries such as "a + 1"
+matOfStrings = replace(string(mat), " ", "")
+% This is a hacky but quick way of joining
+% entries with spaces, joining lines with semicolons,
+% and wrapping the whole things withs square bracket.
+% It should probably be replaced% with more careful code ...
+stringRep = replace(mat2str(matOfStrings), '"', '')
