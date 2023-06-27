6ff3d3a4c9e27059298107bc73e6a140b8732748
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2023-06-27 15:07:06 -0400

    Changed stringToMatrix to produce sym matrix
    Previously, stringToMatrix was "optimized" to return a numeric matrix
    when the input had no letters, i.e., variables.
    While this would preserve the forms of matrices with only integer entries,
    matrices with no variables + rational entries would
    be turned into double matrices too, e.g., "[1/3]" -> [0.333333].

--- mlfunctions/stringToMatrix.m
+++ mlfunctions/stringToMatrix.m
@@ -1,11 +1,14 @@
 function mat = stringToMatrix(string)
 
+% NOTE: The following also changes fractions into doubles ...
 % saves time to avoid unnecessary symbols
-if hasSymbols(string)
-    mat = str2sym(string)
-else
-    mat = str2num(string)
-end
+%if hasSymbols(string)
+%    mat = str2sym(string)
+%else
+%    mat = str2num(string)
+%end
+
+mat = str2sym(string)
 
 function val = hasSymbols(string)
     matches = regexp(string, "[a-zA-Z]")
